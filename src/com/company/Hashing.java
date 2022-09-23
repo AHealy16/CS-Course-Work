cfc19be9bd17cc62df74031079b26694101ec6bf
AHealy16 <AH211841@reigate.ac.uk>
2022-09-23 13:26:57 +0100

    Done some more hashing, the main problem is getting the password and running it through my hash.

--- src/com/company/Hashing.java
+++ src/com/company/Hashing.java
@@ -1,54 +1,23 @@
 package com.company;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 //https://www.geeksforgeeks.org/hashtable-in-java/
+//https://www.tutorialspoint.com/java_cryptography/java_cryptography_message_digest.htm
 
 public class Hashing {
 
-//    public void Hash
-//
-//    {
-//
-//
-//        private static String getSecurePassword (String, String){
-//        String generatedPassword = null;
-//        try {
-//
-//            MessageDigest md = MessageDigest.getInstance("MD5");
-//
-//            md.update(salt.getBytes());
-//
-//
-//            byte[] bytes = md.digest(passwordToHash.getBytes());
-//
-//            StringBuilder sb = new StringBuilder();
-//
-//            for (int i = 0; i < bytes.length; i++) {
-//                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
-//                        .substring(1));
-//            }
-//
-//            generatedPassword = sb.toString();
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//        return generatedPassword;
-//    }
-//
-//
-//        private static String getSalt () throws NoSuchAlgorithmException, NoSuchProviderException {
-//
-//        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
-//
-//        byte[] salt = new byte[16];
-//
-//        sr.nextBytes(salt);
-//
-//        return salt.toString();
-//    }
-//    }
+    User findPassword = new User();
+
+    String password = findPassword.getPassword();
+
+    private Hashing() throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes(StandardCharsets.UTF_8));
+
+    }
 }
 
 
